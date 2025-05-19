68d8d2d4f355677c16b85ab8edd8f351ca4f55d1
kayubin <kayubin@gmail.com>
2025-05-19 19:25:27 +0900

    Add requirements.txt and static

--- postboxProject/postboxProject/urls.py
+++ postboxProject/postboxProject/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
 ]
+
+handler404 = views.page_not_found
